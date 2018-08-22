40b1308f73ee32631cb74d7c2cf8c256ef4297da
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2018-08-22 16:12:53 +0100

    feat(geojson): Add GeoJSON representation of data
    * Python script to turn canonical CSV into GeoJSON
    * Initial version of GeoJSON generated from CSV

---
+++ csv-to-geojson.py
@@ -0,0 +1,43 @@
+import csv
+import json
+
+
+def read_csv():
+    with open('receptacles.csv') as f:
+        return list(csv.DictReader(f))
+
+
+def row_to_point(row):
+    return {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                float(row['longitude']),
+                float(row['latitude'])
+            ]
+        },
+        'properties': {
+            'general': int(row['general']),
+            'recycle_plastic': int(row['recycle_plastic']),
+            'recycle_paper': int(row['recycle_paper']),
+            'dog': int(row['dog'])
+
+        }
+    }
+
+
+def generate_geojson():
+    return {
+        'type': 'FeatureCollection',
+        'features': [row_to_point(row) for row in read_csv()]
+    }
+
+
+def write_to_file(output):
+    with open('receptacles.geojson', "w") as schema:
+        schema.write(json.dumps(output, indent=4))
+
+
+if __name__ == '__main__':
+    write_to_file(generate_geojson())
